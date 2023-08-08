05291be11cdf2a81f15ad7b93b47c244dc7b18e4
Chen Pei <cp0613@linux.alibaba.com>
2023-08-08 14:15:33 +0800

    platform/generic:add thead, copy from allwinner
    Signed-off-by: Chen Pei <cp0613@linux.alibaba.com>

---
+++ platform/generic/thead/objects.mk
@@ -0,0 +1,6 @@
+#
+# SPDX-License-Identifier: BSD-2-Clause
+#
+
+carray-platform_override_modules-$(CONFIG_PLATFORM_THEAD_C9XX) += thead_c9xx
+platform-objs-$(CONFIG_PLATFORM_THEAD_C9XX) += thead/c9xx.o
